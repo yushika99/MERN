335fe0c20167e4b2c4afada9eae3cfcbc7520750
yushika99 <it20105952@my.sliit.lk>
2022-05-11 10:09:45 +0530

    edit 2

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require ('express');
 const mongoose = require('mongoose');
 const todoitems = require('./models/todoitems');
-const dotenv = require ('dotenv').config;
+const dotenv = require ('dotenv').config ({path:'/.env'});
+
 
 const app = express();
 //use express.jso() to get data into the json format
@@ -13,12 +14,17 @@ app.use (express.json());
 const PORT = process.env.PORT || 5500;
 //import routers
  const TodoItemRoute = require('./routes/todoitems');
+console.log(process.env.MONGOURL)
+console.log(process.env.PORTL)
 
 //connect to mongodb
-mongoose.connect(process.env.DB_CONNECT)
+mongoose.connect("mongodb+srv://yushika:1234yushika@cluster0.gqkve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=> console.log("database connect"))
 .catch(err=> console.log(err))
+;
+
 // add port connecting to the server.
 app.listen(PORT, ()=> console.log("Server connected"));
 
 app.use('/',TodoItemRoute);
+
--- server/routes/todoitems.js
+++ server/routes/todoitems.js
@@ -11,7 +11,43 @@ router.post('/api/item', async (req,res)=>{
       const saveItem = await newItem.save()
       res.status(200).json('item added successfuly')
     } catch (error) {
-        res.json(err)
+        res.json(err);
+    }
+})
+
+//second route get data from data base
+router.get('/api/items', async (req,res)=>{
+    try {
+      const allTodoItems = await todoItemsModel.find({}) 
+      
+      res.status(200).json(allTodoItems) //retrn all items in dabase to json fromate
+    } catch (error) {
+        res.json(err);
+    }
+})
+
+
+//update items
+router.put('/api/item/:id', async (req,res)=>{
+    try {
+      //find item by id
+      const updateItem = todoItemsModel.findByIdAndUpdate(req.params.id,{$set:req.body}); 
+      
+      res.status(200).json('Item updated') //retrn all items in dabase to json fromate
+    } catch (error) {
+        res.json(err);
+    }
+})
+
+//delete items
+router.delete('/api/item/:id', async (req,res)=>{
+    try {
+      //find item by id and delete
+      const deleteItem = await todoItemsModel.findByIdAndDelete(req.params.id); 
+      
+      res.status(200).json('Item Deleted') //retrn all items in dabase to json fromate
+    } catch (error) {
+        res.json(err);
     }
 })
 
